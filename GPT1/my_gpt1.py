451d0cc38aeb0a599cb2b1668e4bde07e765181d
Kodp <954614718@QQ.COM>
2024-11-26 21:24:23 +0800

    Download tinyshakespeare, Add tokenizer

--- GPT1/my_gpt1.py
+++ GPT1/my_gpt1.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
+#@ 0. 超参数
 batch_size    = 64               # 批大小
 context_len   = block_size = 256 # 上下文长度（注意力矩阵NxN,N的大小）
 max_iters     = 5000 # 一共迭代次数
@@ -15,4 +16,21 @@ dropout       = 0.2  # dropout 比例
 
 gpu_index     = 0
 device        = f'cuda:{gpu_index}' if torch.cuda.is_available() else 'cpu'
+
+torch.manual_seed(1337)
+
+
+#@ 数据集
+# wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
+with open('input.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+    
+#@ tokenizer
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+stoi = { ch:i for i,ch in enumerate(chars) }
+itos = { i:ch for i,ch in enumerate(chars) }
+
+encode = lambda text_seq: [stoi[c] for c in text_seq]
+decode = lambda idx_seq: ''.join([itos[i] for i in idx_seq])
 
